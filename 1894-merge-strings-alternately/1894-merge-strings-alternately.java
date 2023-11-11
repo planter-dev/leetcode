98c2912835ec2def5d7d01e4eeeeae3f9432b40e
planter-dev <109575389+planter-dev@users.noreply.github.com>
2023-11-11 17:29:06 +0900

    Time: 1 ms (78.36%), Space: 40.8 MB (54.04%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,36 +1,18 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-
-        List<String> word1Array = split(word1);
-        List<String> word2Array = split(word2);
-
-        return mergeLists(word1Array, word2Array);
-    };
-
-    private List<String> split(String word) {
-        String[] wordArray = word.replaceAll(" ", "").split("");
-        return Arrays.asList(wordArray);
-    }
-
-    private String mergeLists(List<String> list1, List<String>list2) {
-        int list1Size = list1.size();
-        int list2Size = list2.size();
-                StringBuilder result = new StringBuilder();
-        if(list1Size >= list2Size) {
-            for(int i = 0; i < list2Size; i ++) {
-                result.append(list1.get(i)).append(list2.get(i));
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        while(i < word1.length() || i < word2.length()) {
+            if(i < word1.length()) {
+                result.append(word1.charAt(i));
             }
-            String endString = String.join("", list1.subList(list2Size, list1Size));
-            result.append(endString);
-            return result.toString();
+            if(i < word2.length()) {
+                result.append(word2.charAt(i));
+            }
+            i++;
         }
-        for(int i = 0; i < list1Size; i ++) {
-            result.append(list1.get(i)).append(list2.get(i));
-        }
-        String endString = String.join("", list2.subList(list1Size, list2Size));
-        result.append(endString);
         return result.toString();
-
-    }
+        
+    };
 
 }
